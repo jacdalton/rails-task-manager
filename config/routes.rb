32ff4ce0b59ca9d665c5fdff70ccebe3d9794ef7
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-06 12:56:07 +0900

    Edit the tasks from the list, including marking them as complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index', as: :tasks
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   
   post 'tasks', to: 'tasks#create'
+  
+  patch 'tasks/:id', to: 'tasks#update'
 end
