629d25d84b97db82e54fb7ee24ebb4d14fc7ae91
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-06 13:09:34 +0900

    Delete individual task with confirmation warning

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   
   post 'tasks', to: 'tasks#create'
-  
+
   patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
